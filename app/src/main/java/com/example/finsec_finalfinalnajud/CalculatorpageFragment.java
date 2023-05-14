ee44feab79119f7c618b74becf75309197e4c0ae
Jomar Magdalaga <jmagdalaga5@gmail.com>
2023-05-14 20:12:27 +0800

    Changes + calc partial

--- app/src/main/java/com/example/finsec_finalfinalnajud/CalculatorpageFragment.java
+++ app/src/main/java/com/example/finsec_finalfinalnajud/CalculatorpageFragment.java
@@ -1,19 +1,128 @@
 package com.example.finsec_finalfinalnajud;
 
+
+import android.app.Activity;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.button.MaterialButton;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CalculatorpageFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CalculatorpageFragment extends Fragment {
+public class CalculatorpageFragment extends Fragment implements View.OnClickListener{
+    TextView resultTv,solutionTv;
+    MaterialButton buttonC,buttonBrackOpen,buttonBrackClose;
+    MaterialButton buttonDivide,buttonMultiply,buttonPlus,buttonMinus,buttonEquals;
+    MaterialButton button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
+    MaterialButton buttonAC,buttonDot;
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setContentView(R.layout.calculatorpage);
+        resultTv = view.findViewById(R.id.result_tv);
+        solutionTv = view.findViewById(R.id.solution_tv);
+
+        assignId(buttonC,R.id.button_c2);
+        assignId(buttonBrackOpen,R.id.button_open_bracket2);
+        assignId(buttonBrackClose,R.id.button_close_bracket2);
+        assignId(buttonDivide,R.id.button_divide2);
+        assignId(buttonMultiply,R.id.button_multiply);
+        assignId(buttonPlus,R.id.button_plus);
+        assignId(buttonMinus,R.id.button_minus);
+        assignId(buttonEquals,R.id.button_equals);
+        assignId(button0,R.id.button_0);
+        assignId(button1,R.id.button_1);
+        assignId(button2,R.id.button_2);
+        assignId(button3,R.id.button_3);
+        assignId(button4,R.id.button_4);
+        assignId(button5,R.id.button_5);
+        assignId(button6,R.id.button_6);
+        assignId(button7,R.id.button_7);
+        assignId(button8,R.id.button_8);
+        assignId(button9,R.id.button_9);
+        assignId(buttonAC,R.id.button_ac);
+        assignId(buttonDot,R.id.button_dot);
+
+
+
+
+
+    }
+
+    private void assignId(MaterialButton buttonDivide, int button_divide2) {
+    }
+
+    private void setContentView(int calculatorpage) {
+
+    }
+
+    void assignId(Activity activity, MaterialButton btn, int id) {
+        btn = activity.findViewById(id);
+        btn.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        MaterialButton button =(MaterialButton) view;
+        String buttonText = button.getText().toString();
+        String dataToCalculate = solutionTv.getText().toString();
+
+        if(buttonText.equals("AC")){
+            solutionTv.setText("");
+            resultTv.setText("0");
+            return;
+        }
+        if(buttonText.equals("=")){
+            solutionTv.setText(resultTv.getText());
+            return;
+        }
+        if(buttonText.equals("C")){
+            dataToCalculate = dataToCalculate.substring(0,dataToCalculate.length()-1);
+        }else{
+            dataToCalculate = dataToCalculate+buttonText;
+        }
+        solutionTv.setText(dataToCalculate);
+
+        String finalResult = getResult(dataToCalculate);
+
+        if(!finalResult.equals("Err")){
+            resultTv.setText(finalResult);
+        }
+
+    }
+
+    String getResult(String data){
+        try{
+            Context context  = Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable = context.initStandardObjects();
+            String finalResult =  context.evaluateString(scriptable,data,"Javascript",1,null).toString();
+            if(finalResult.endsWith(".0")){
+                finalResult = finalResult.replace(".0","");
+            }
+            return finalResult;
+        }catch (Exception e){
+            return "Err";
+        }
+    }
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -46,14 +155,7 @@ public class CalculatorpageFragment extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -61,4 +163,6 @@ public class CalculatorpageFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.calculatorpage, container, false);
     }
+
+
 }

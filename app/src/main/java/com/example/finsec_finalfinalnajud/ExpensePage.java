1508541de2ab69163eee98260314504f72314b38
Jomar Magdalaga <jmagdalaga5@gmail.com>
2023-05-16 17:47:40 +0800

    Adjust in Calc UI + Advisory Page Done + Expense Page

---
+++ app/src/main/java/com/example/finsec_finalfinalnajud/ExpensePage.java
@@ -0,0 +1,14 @@
+package com.example.finsec_finalfinalnajud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExpensePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.expense_page);
+    }
+}
